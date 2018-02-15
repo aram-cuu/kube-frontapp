ef5621965aa49e30a2af825eda9e0c60c47dabea
Alberto Aram Rascón García <aram123@gmail.com>
2018-02-15 02:15:52 -0600

    Initial commit

---
+++ www/get-busy.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * Created by Alberto Aram Rascón García
+ * Project: kube-frontapp
+ */
+echo file_get_contents("http://172.21.0.5/get-busy.php");
---
+++ www/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<a href="fetch-message.php">Fetch message</a>
+<a href="get-busy.php">Get busy</a>
+<?php echo $_SERVER['SERVER_ADDR'];?>
+</body>
+</html>
